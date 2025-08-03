4261eabd5a9758c065762d320ffc50d6bb34fba7
ziggle <muyue1125@gmail.com>
2025-08-03 16:06:59 +0800

    init

--- src/main/java/com/example/generator/generators/RepositoryGenerator.java
+++ src/main/java/com/example/generator/generators/RepositoryGenerator.java
@@ -4,9 +4,10 @@ import com.example.generator.CodeGenerator;
 import com.example.generator.model.PackageConfig;
 import com.example.generator.model.PojoInfo;
 import com.squareup.javapoet.ClassName;
+import com.squareup.javapoet.CodeBlock;
+import com.squareup.javapoet.MethodSpec;
 import com.squareup.javapoet.ParameterizedTypeName;
 import com.squareup.javapoet.TypeSpec;
-import com.squareup.javapoet.MethodSpec;
 import com.mybatisflex.core.query.QueryWrapper;
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,50 +27,55 @@ public class RepositoryGenerator implements CodeGenerator {
 
     @Override
     public TypeSpec generate(PojoInfo pojoInfo) {
-        // 获取实体类名
         String entityName = pojoInfo.getClassName();
-        // 创建实体类类型
         ClassName entityType = ClassName.get(pojoInfo.getPackageName(), entityName);
-        // 创建MyBatis Flex的BaseMapper接口类型
         ClassName baseMapperType = ClassName.get("com.mybatisflex.core", "BaseMapper");
-        // 创建参数化的BaseMapper类型
         ParameterizedTypeName baseMapperOfEntity = ParameterizedTypeName.get(baseMapperType, entityType);
 
-        // 创建接口构建器
         TypeSpec.Builder interfaceBuilder = TypeSpec.interfaceBuilder(getClassName(pojoInfo))
                 .addModifiers(Modifier.PUBLIC)
                 .addSuperinterface(baseMapperOfEntity);
 
-        // 添加接口注释
         if (pojoInfo.getClassComment() != null && !pojoInfo.getClassComment().isEmpty()) {
             interfaceBuilder.addJavadoc(pojoInfo.getClassComment() + "\n");
             interfaceBuilder.addJavadoc("数据访问层接口\n");
         }
 
-        // 添加selectListByQuery方法
-        MethodSpec selectListByQuery = MethodSpec.methodBuilder("selectListByQuery")
+        ClassName tableRefs = ClassName.get(pojoInfo.getPackageName() + ".table", pojoInfo.getClassName() + "TableRefs");
+        String tableVar = pojoInfo.getClassName().toLowerCase() + "Table";
+        String staticTableField = pojoInfo.getClassName().toUpperCase();
+
+        MethodSpec.Builder methodBuilder = MethodSpec.methodBuilder("selectListByQuery")
                 .addModifiers(Modifier.PUBLIC, Modifier.DEFAULT)
                 .addParameter(ClassName.get(packageConfig.getRequestPackage(), pojoInfo.getClassName() + "Query"), "query")
-                .returns(ParameterizedTypeName.get(ClassName.get(List.class), entityType))
-                .addStatement("QueryWrapper queryWrapper = QueryWrapper.create()")
-                .addCode(buildQuery(pojoInfo))
-                .addStatement("return selectListByQuery(queryWrapper)")
-                .build();
-        interfaceBuilder.addMethod(selectListByQuery);
+                .returns(ParameterizedTypeName.get(ClassName.get(List.class), entityType));
+
+        methodBuilder.addStatement("var $L = $T.$L", tableVar, tableRefs, staticTableField);
+
+        CodeBlock.Builder queryBuilder = CodeBlock.builder();
+        queryBuilder.add("$T queryWrapper = $T.create()\n", QueryWrapper.class, QueryWrapper.class);
+        queryBuilder.indent();
+        queryBuilder.add(".from($L)\n", tableVar);
+
+        for (PojoInfo.FieldInfo field : pojoInfo.getFields()) {
+            queryBuilder.add(".where($L.$L.eq(query.get$L(), query.get$L() != null))\n",
+                    tableVar, field.getName().toUpperCase(), upperFirstChar(field.getName()), upperFirstChar(field.getName()));
+        }
+        
+        String idField = pojoInfo.getFields().stream()
+            .map(PojoInfo.FieldInfo::getName)
+            .filter(name -> "id".equalsIgnoreCase(name))
+            .findFirst()
+            .orElse("id").toUpperCase();
+
+        queryBuilder.add(".orderBy($L.$L.desc());\n", tableVar, idField);
+        queryBuilder.unindent();
+
+        methodBuilder.addCode(queryBuilder.build());
+        methodBuilder.addStatement("return selectListByQuery(queryWrapper)");
+        interfaceBuilder.addMethod(methodBuilder.build());
 
         return interfaceBuilder.build();
-    }
-
-    private String buildQuery(PojoInfo pojoInfo) {
-        StringBuilder queryBuilder = new StringBuilder();
-        for (PojoInfo.FieldInfo field : pojoInfo.getFields()) {
-            queryBuilder.append(String.format("if (query.get%s() != null) {\n", 
-                upperFirstChar(field.getName())));
-            queryBuilder.append(String.format("    queryWrapper.where(%s.%s.eq(query.get%s()));\n", 
-                pojoInfo.getClassName().toUpperCase(), field.getName().toUpperCase(), upperFirstChar(field.getName())));
-            queryBuilder.append("}\n");
-        }
-        return queryBuilder.toString();
     }
 
     private String upperFirstChar(String str) {
@@ -78,8 +84,6 @@ public class RepositoryGenerator implements CodeGenerator {
         }
         return Character.toUpperCase(str.charAt(0)) + str.substring(1);
     }
-
-
 
     @Override
     public String getPackageName() {

345f382df1269d57db9ca65f028d48e90ff6b964
ziggle <muyue1125@gmail.com>
2025-09-27 01:01:17 +0800

    Revert "refactor(pom): remove example module from project configuration"
    This reverts commit d3fdb0253fcae6d12af3c6282ad9af901c0b955a.

---
+++ example/sub-module/src/main/java/abc/CodeGeneratorMain.java
@@ -0,0 +1,31 @@
+package abc;
+
+import abc.entity.User;
+import io.github.youngerier.generator.GeneratorConfig;
+import io.github.youngerier.generator.GeneratorEngine;
+import lombok.extern.slf4j.Slf4j;
+
+import java.io.File;
+import java.util.Collections;
+
+/**
+ * 代码生成器主程序 - 示例
+ */
+@Slf4j
+public class CodeGeneratorMain {
+
+    public static void main(String[] args) {
+        // 1. 定义配置
+        Class<?> pojoClass = User.class;
+
+        // 2. 构建 GeneratorConfig
+        GeneratorConfig config = GeneratorConfig.builder()
+                .outputBaseDir("example/sub-module/target" + File.separator + "generated-sources")
+                .pojoClasses(Collections.singletonList(pojoClass))
+                .build();
+
+        // 3. 创建并执行引擎
+        GeneratorEngine engine = new GeneratorEngine(config);
+        engine.execute();
+    }
+}
---
+++ example/sub-module/src/main/java/abc/entity/User.java
@@ -0,0 +1,48 @@
+package abc.entity;
+
+import io.github.youngerier.generator.annotation.GenModel;
+import com.mybatisflex.annotation.Id;
+import com.mybatisflex.annotation.Table;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+/**
+ * 用户实体
+ */
+@Table(value = User.TABLE_NAME)
+@Data
+@GenModel
+public class User {
+    public static final String TABLE_NAME = "t_user";
+
+    /**
+     * 主键
+     */
+    @Id
+    private Long id;
+
+    /**
+     * 用户名
+     */
+    private String username;
+    /**
+     * 密码
+     */
+    private String password;
+
+    /**
+     * 用户类型
+     */
+    private UserTypeEnum userType;
+
+    /**
+     * 创建时间
+     */
+    private LocalDateTime gmtCreate;
+
+    /**
+     * 修改时间
+     */
+    private LocalDateTime gmtModified;
+}

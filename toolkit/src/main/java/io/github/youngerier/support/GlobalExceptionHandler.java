dca7e68775cc2e2bc8077876caba84e3b114f75a
ziggle <muyue1125@gmail.com>
2025-10-13 22:42:36 +0800

    refactor(GlobalExceptionHandler): remove unused imports and clean up code

--- toolkit/src/main/java/io/github/youngerier/support/GlobalExceptionHandler.java
+++ toolkit/src/main/java/io/github/youngerier/support/GlobalExceptionHandler.java
@@ -1,16 +1,7 @@
 package io.github.youngerier.support;
 
-import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
-import org.springframework.web.context.request.RequestContextHolder;
-import org.springframework.web.context.request.ServletRequestAttributes;
-
-import java.util.UUID;
 
 
 @Slf4j
